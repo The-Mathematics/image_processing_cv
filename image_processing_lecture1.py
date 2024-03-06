3bfa6bf2d63c13376f79356b123681f802ab1b12
Onuah Gift <161401660+The-Mathematics@users.noreply.github.com>
2024-03-06 09:27:03 -0800

    Update image_processing_lecture1.py

--- image_processing_lecture1.py
+++ image_processing_lecture1.py
@@ -14,6 +14,11 @@ cv2.imshow("Gray Image1", image1Gray)
 #This code line converts the image1 to grayscale and assigns it to a new variable image1Gray
 #This code line displays the greyed image with the name "Gray Image1"
 
+image1Blur = cv2.GaussianBlur(image1, (13,13), 0)
+cv2.imshow("Blurred Image1", image1Blur)
+#This code line blurs the image using the gaussian blur technique,it requires that you provide the ksize and sigmaX and sigmaY - if sigmaY is not provided,it will assume the same value as sigmaX - if both values are 0,they will be calculated from the kernel size
+#This line displays the blurred image with the name "Blurred Image1"
+
 image1Canny = cv2.Canny(image1, 150, 200)
 cv2.imshow("Edges Image1", image1Canny)
 #This code line converts the image1 to a tresh image to reveal its edges and assigns it to a new variable image1Canny
@@ -32,9 +37,11 @@ print("Resized Dimension:", image1Resize.shape)
 cv2.imwrite("Gray Image1.jpeg", image1Gray)
 cv2.imwrite("Edges Image1.jpeg", image1Canny)
 cv2.imwrite("Resized Image1.jpeg", image1Resize)
+cv2.imwrite("Blurred Image1.jpeg", image1Blur)
 cv2.waitKey(0)
 #This code line saves the greyed image as "Gray Image1"
 #This code line saves the Treshed image as "Edges Image1"
 #This code line saves the Resized image as "Resized Image1"
+#This code line saves the Blurred image as "Blurred Image1"
 #This code line sets the delay for the code to run
 
