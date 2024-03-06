0de800277206a8015ae54ece5421119259cfd995
Onuah Gift <161401660+The-Mathematics@users.noreply.github.com>
2024-03-05 17:54:25 -0800

    Add files via upload

---
+++ image_processing_lecture1.py
@@ -0,0 +1,40 @@
+import cv2
+
+image1 = cv2.imread("image1.jpeg")
+#This code line creates a variable image1 and assigns the value "image.jpeg" to it
+
+cv2.imshow("Normal Image1", image1)
+#This code line displays the image with a new name "Normal Image"
+
+cv2.imshow("image1.jpeg", image1)
+#This code line displays the original image
+
+image1Gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Image1", image1Gray)
+#This code line converts the image1 to grayscale and assigns it to a new variable image1Gray
+#This code line displays the greyed image with the name "Gray Image1"
+
+image1Canny = cv2.Canny(image1, 150, 200)
+cv2.imshow("Edges Image1", image1Canny)
+#This code line converts the image1 to a tresh image to reveal its edges and assigns it to a new variable image1Canny
+#This code line displays the treshed image with the name "Edges Image1"
+
+print("Original Dimension:", image1.shape)
+image1Resize = cv2.resize(image1, [200,200])
+cv2.imshow("Resized Image1", image1Resize)
+print("Resized Dimension:", image1Resize.shape)
+#This code line prints out the dimensions for the original image stored in the variable image1
+#This code line resizes the image1 to a dsize [200,200] and assigns it to a new variable image1Resize
+#This code line displays the resized image with the name "Resized Image1"
+#This code line prints out the dimensions for the resized image stored in image1Resize
+
+
+cv2.imwrite("Gray Image1.jpeg", image1Gray)
+cv2.imwrite("Edges Image1.jpeg", image1Canny)
+cv2.imwrite("Resized Image1.jpeg", image1Resize)
+cv2.waitKey(0)
+#This code line saves the greyed image as "Gray Image1"
+#This code line saves the Treshed image as "Edges Image1"
+#This code line saves the Resized image as "Resized Image1"
+#This code line sets the delay for the code to run
+
